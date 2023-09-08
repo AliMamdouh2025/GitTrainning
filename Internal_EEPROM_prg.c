7ea728f5b85601021090aa9efb9c78e0349cc75b
Ali_Mamdouh <frinfkk@gmail.com>
2023-09-08 11:02:01 +0300

    Initializing PWM to 100

--- Internal_EEPROM_prg.c
+++ Internal_EEPROM_prg.c
@@ -32,7 +32,7 @@
 #include "Internal_EEPROM_private.h"
 
 uint8 x;
-uint16 PWMVAL;
+uint16 PWMVAL = 100;
 
 /***********************************[1]***********************************************
  * Service Name: MADC_VidGetSWVersion
